644e419144df3533c8a961200d6b45dcf9337288
sowndharyaraja2203 <sowndharyaraja2203@gmail.com>
2026-09-20 21:08:43 +0530

    File updated at 20/09/2026, 21:08:43

--- 136-SingleNumber/136-SingleNumber.java
+++ 136-SingleNumber/136-SingleNumber.java
@@ -1,9 +1,9 @@
-// Last updated: 20/09/2026, 21:08:08
+// Last updated: 20/09/2026, 21:08:43
 1class Solution {
 2    public int singleNumber(int[] nums) {
 3        int res=0;
-4        for(int num:nums){
-5            res^=num;
+4        for(int i=0;i<nums.length;i++){
+5            res^=nums[i];
 6        }
 7        return res;
 8    }
