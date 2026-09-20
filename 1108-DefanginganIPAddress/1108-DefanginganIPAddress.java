4b92a3e88a696c4b78150ad0e2e4b1e28fabda48
sowndharyaraja2203 <sowndharyaraja2203@gmail.com>
2026-09-20 21:06:17 +0530

    File updated at 20/09/2026, 21:06:17

--- 1108-DefanginganIPAddress/1108-DefanginganIPAddress.java
+++ 1108-DefanginganIPAddress/1108-DefanginganIPAddress.java
@@ -1,6 +1,6 @@
-// Last updated: 20/09/2026, 21:05:40
+// Last updated: 20/09/2026, 21:06:17
 1class Solution {
 2    public String defangIPaddr(String address) {
-3        return address.replace(".","[.]");        
+3        return address.replace(".","[.]");       
 4    }
 5}
