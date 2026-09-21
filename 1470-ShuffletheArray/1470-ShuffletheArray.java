ff379d1dedbc9e16d78cf94e45f2fb73b03f7bc1
sowndharyaraja2203 <sowndharyaraja2203@gmail.com>
2026-09-21 21:31:22 +0530

    File updated at 21/09/2026, 21:31:22

--- 1470-ShuffletheArray/1470-ShuffletheArray.java
+++ 1470-ShuffletheArray/1470-ShuffletheArray.java
@@ -1,14 +1,15 @@
-// Last updated: 21/09/2026, 21:29:47
+// Last updated: 21/09/2026, 21:31:22
 1class Solution {
 2    public int[] shuffle(int[] nums, int n) {
-3        int[] res=new int[2*n];
-4        int index=0;
-5        for(int i=0;i<n;i++){
-6            res[index]=nums[i];
-7            index++;
-8            res[index]=nums[i+n];
-9            index++;
-10        } 
-11        return res;       
+3       int[] res=new int[2*n];
+4       int index=0;
+5       for(int i=0;i<n;i++){
+6        res[index]=nums[i];
+7        index++;
+8        res[index]=nums[i+n];
+9        index++;
+10       }
+11       return res;
 12    }
 13}
+14
