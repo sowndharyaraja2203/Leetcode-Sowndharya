292f96ad3223273f8e7a4d43ab0c6e4182a4af10
sowndharyaraja2203 <sowndharyaraja2203@gmail.com>
2026-09-24 20:43:31 +0530

    File updated at 24/09/2026, 20:43:31

--- 704-BinarySearch/704-BinarySearch.java
+++ 704-BinarySearch/704-BinarySearch.java
@@ -1,4 +1,4 @@
-// Last updated: 24/09/2026, 20:41:38
+// Last updated: 24/09/2026, 20:43:31
 1class Solution {
 2    public int search(int[] nums, int target) {
 3        int left=0;
@@ -6,7 +6,7 @@
 5        while(left<=right){
 6            int mid=left+(right-left)/2;
 7            if(nums[mid]==target){
-8                 return mid;
+8                return mid;
 9            }else if(nums[mid]<target){
 10                left=mid+1;
 11            }else{
