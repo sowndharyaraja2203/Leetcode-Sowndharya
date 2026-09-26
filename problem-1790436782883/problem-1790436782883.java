bc57d6934054c4d8c163cd4af1c917c2ac0337b8
sowndharyaraja2203 <sowndharyaraja2203@gmail.com>
2026-09-26 21:03:04 +0530

    Create

---
+++ problem-1790436782883/problem-1790436782883.java
@@ -0,0 +1,14 @@
+// Last updated: 26/09/2026, 21:03:02
+1class Solution {
+2    public boolean canTransform(int[] source, int[] target) {
+3        long sum1=0;
+4        long sum2=0;
+5        for(int x:source){
+6            sum1+=x;
+7        }
+8        for(int x:target){
+9            sum2+=x;
+10        }
+11        return sum1==sum2;        
+12    }
+13}
